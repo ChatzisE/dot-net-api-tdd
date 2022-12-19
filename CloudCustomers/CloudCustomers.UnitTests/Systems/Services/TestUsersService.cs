1bdd3c9bb5fba0001d018446652a7d5feda2f0c0
Evangelos Chatzis <tzisxa1@gmail.com>
2022-12-19 12:15:19 +0200

    *Implement GetAllUsers

--- CloudCustomers/CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
+++ CloudCustomers/CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -2,21 +2,48 @@ using CloudCustomers.API.Controllers;
 using CloudCustomers.API.Models;
 using CloudCustomers.API.Services;
 using CloudCustomers.UnitTests.Fixtures;
+using CloudCustomers.UnitTests.Helpers;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Moq.Protected;
 
 namespace CloudCustomers.UnitTests.Systems.Services;
 
 public class TestUsersService
 {
-    [Fact] public async Task GetAllUsers_WhenCalled_InvokesHttpGetRequest()
+    [Fact]
+    public async Task GetAllUsers_WhenCalled_InvokesHttpGetRequest()
     {
         // Arrange
-        //var sut = new UsersService();
+        var expectedResponse = UsersFixture.GetTestUsers();
+        var handlerMock = MockHttpMessageHandler<User>.SetupBasicAGetResourceList((expectedResponse));
+        var httpClient = new HttpClient(handlerMock.Object);
+        var sut = new UsersService(httpClient);
         // Act
         await sut.GetAllUsers();
         // Assert
         // Verify HTTP request is made!
+        handlerMock
+            .Protected()
+            .Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
+                ItExpr.IsAny<CancellationToken>());
+    }
+    
+    [Fact]
+    public async Task GetAllUsers_WhenCalled_ReturnsListOfUsers()
+    {
+        // Arrange
+        var expectedResponse = UsersFixture.GetTestUsers();
+        var handlerMock = MockHttpMessageHandler<User>.SetupBasicAGetResourceList((expectedResponse));
+        var httpClient = new HttpClient(handlerMock.Object);
+        var sut = new UsersService(httpClient);
+        // Act
+       var result = await sut.GetAllUsers();
+        // Assert
+        result.Should().BeOfType<List<User>>();
     }
 }
